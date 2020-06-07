9da30135c0569cc6409e9975c4b1861316f7e95d
Phide12 <49126676+Phide12@users.noreply.github.com>
2020-06-07 20:29:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ function loadCube() {
 
 function startDrag(event) {
   cubeElement.isMoving = true;
-  let currentX = event.touches[0].clientX : event.pageX;
-  let currentY = event.touches[0].clientY : event.pageY;
+  let currentX = event.touches ? event.touches[0].clientX : event.pageX;
+  let currentY = event.touches ? event.touches[0].clientY : event.pageY;
   cubeElement.startPosX = currentX + rotacionX;
   cubeElement.startPosY = currentY + rotacionY;
   document.querySelector('.cube').classList.toggle("rotate");
